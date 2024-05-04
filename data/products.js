fbcab7d294cedee85cfe10c635ab584d0f6b70f0
krishan <mondalkrishan638@gmail.com>
2024-05-04 21:36:38 +0530

    Use inhertance and polymorphism

--- data/products.js
+++ data/products.js
@@ -24,11 +24,32 @@ class Products {
   }
 
   getstarsUrl() {
-   return  `images/ratings/rating-${this.rating.stars * 10}.png`;
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
   }
 
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML() {
+    return ``;
+  }
+}
+
+class Clothing extends Products {
+  sizeChartLink;
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a> 
+      `;
   }
 }
 
@@ -504,6 +525,6 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") return new Clothing(productDetails);
   return new Products(productDetails);
 });
-
